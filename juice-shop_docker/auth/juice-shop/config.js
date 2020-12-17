153ed878bc453100aa59272b00172e3e09394f5a
skyworld42 <trongnhan061199@gmail.com>
2020-12-17 16:03:38 +1000

    Added auth script for juice-shop

---
+++ juice-shop_docker/auth/juice-shop/config.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+module.exports = {
+    // Post-processing of responses
+    response: (resp, options, ctx) => {
+        let body = JSON.parse(resp.body);
+
+        // The following path is used by the restfuzz command of test-juice.py 
+        fs.writeFileSync(path.join("./", 'juiceshop_auth_token.txt'), `Bearer ${body.authentication.token}`);
+    }
+}
